0f3e109bf26c6e62bc9cc12d9710d1f481ad6afe
iharzh <iharzhuk22@gmail.com>
2021-09-05 20:47:09 +0200

    Added basic routing handling

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees. Limit: ${limit}. Offset: ${offset}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action return ${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action deletes #${id} coffee`;
+  }
+}
