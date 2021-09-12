8a0fa0930e6860ca569fd79afa8983d2c81924a4
iharzh <iharzhuk22@gmail.com>
2021-09-12 22:01:45 +0200

    Created timeout interceptor

---
+++ src/common/interceptors/timeout.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { TimeoutInterceptor } from './timeout.interceptor';
+
+describe('Timeout.InterceptorInterceptor', () => {
+  it('should be defined', () => {
+    expect(new TimeoutInterceptor()).toBeDefined();
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ApiKeyGuard } from './common/guards/api-key.guard';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +17,10 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
   await app.listen(3000);
 }
 
